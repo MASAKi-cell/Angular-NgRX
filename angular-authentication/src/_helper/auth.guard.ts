09eb491530ec492926802d23d31cda3e28d29423
MASAKi-cell <bje35149@gmail.com>
2022-01-10 22:40:13 +0900

    serviceフォルダの追加

--- angular-authentication/src/_helper/auth.guard.ts
+++ angular-authentication/src/_helper/auth.guard.ts
@@ -12,7 +12,7 @@ export class AuthGuard implements CanActivate {
     }
 
     canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean | UrlTree | Observable<boolean | UrlTree> | Promise<boolean | UrlTree> {
-        this.router.navigate(['/login'], { queryParams: { returnUrl: state.url } })
+        return this.router.navigate(['/login'], { queryParams: { returnUrl: state.url } });
     }
 
 }
