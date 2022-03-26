c20abeeceb70fe37af74f80da2d171dcad569553
masaki.takahashi <bje35149@gmail.com>
2022-03-26 16:46:02 +0900

    副作用の処理

--- ngrx/src/app/app-store/effects/todo.effect.ts
+++ ngrx/src/app/app-store/effects/todo.effect.ts
@@ -1,4 +1,34 @@
-import { Injectable } from "@angular/core";
+import { Injectable } from '@angular/core';
 import { Actions, ofType, createEffect, act } from '@ngrx/effects';
 import { of } from 'rxjs';
 import { map, tap, concatMap, switchMap, catchError } from 'rxjs/operators';
+import { TodoService } from 'src/app/app-store/serevice/todo.service';
+import * as TodoActions from 'src/app/app-store/actions/index';
+
+@Injectable()
+export class TodoEffects {
+
+  constructor(
+      private todoService: TodoService, 
+      private actions$: Actions,
+    ) {}
+
+  /**
+   * Actionを受け取り副作用を処理する。
+   */
+  loadAll$ = createEffect(() =>
+    this.actions$.pipe(
+      ofType(TodoActions.loadAll),
+      switchMap(({ offset, limit }) =>
+        this.todoService.findAll(offset, limit).pipe(
+
+          // 成功の場合はloadSuccess
+          map((result) => TodoActions.loadAllSuccess({ todos: result })),
+
+          // エラーの場合はloadFailure
+          catchError(async (error) => TodoActions.loadAllFailure({ error }))
+        )
+      )
+    )
+  );
+}
