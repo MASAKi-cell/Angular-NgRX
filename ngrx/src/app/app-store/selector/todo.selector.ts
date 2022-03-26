637eab8e8cff2821ffa6f067b3458f7343a65aa1
masaki.takahashi <bje35149@gmail.com>
2022-03-26 15:18:52 +0900

    httpserviceの追加

--- ngrx/src/app/app-store/selector/todo.selector.ts
+++ ngrx/src/app/app-store/selector/todo.selector.ts
@@ -3,7 +3,8 @@ import { State, featureName } from '../state/todo.state';
 
 const getState = createFeatureSelector<State>(featureName); // todo
 
-export const getLoading = createSelector(getState, state => state.loading);
-export const getTodos = createSelector(getState, state => state.todos);
+export const getLoading = createSelector(getState, state => state.loading); // loading
+
+export const getTodos = createSelector(getState, state => state.todos); // todos
 
 
--- ngrx/src/app/app-store/serevice/todo.service.ts
+++ ngrx/src/app/app-store/serevice/todo.service.ts
@@ -1,6 +1,7 @@
-import { Injectable, InjectionToken } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Todo } from 'src/app/app-store/models/todo';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
@@ -10,6 +11,20 @@ export class TodoService {
   constructor(
       private http: HttpClient
     ) {}
+
+    /**
+     * Todoデータを取得する。
+     * @param offset 
+     * @param limit 
+     * @returns Todo[]
+     */
+    findAll(offset?: number, limit?: number): Observable<Todo[]> {
+      const url = 'APIのアクセス先URL';
+      let params = new HttpParams();
+      params = offset ? params.set('offset', `${offset}`) : params;
+      params = limit ? params.set('limit', `${limit}`) : params;
+      return this.http.get<Todo[]>(url { params });
+    }
 
     
 }
