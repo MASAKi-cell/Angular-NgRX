5b78e91610f407e3219f97af158724b5f328995a
masaki.takahashi <bje35149@gmail.com>
2022-03-29 10:31:17 +0900

    reducerの追加(途中)

--- ngrx/src/app/app-store/reducers/todo.reducer.ts
+++ ngrx/src/app/app-store/reducers/todo.reducer.ts
@@ -1,1 +1,8 @@
-import { Action, createReducer, on } from '@ngrx/store'
+import { Action, createReducer, on } from '@ngrx/store';
+import { State, initialState } from 'src/app/app-store/state/todo.state';
+import * as TodoAction from 'src/app/app-store/actions/index';
+
+
+export const reducer = createReducer(initialState, on(TodoAction.loadAll, state => {
+    return {...state, loading: true};
+}))
